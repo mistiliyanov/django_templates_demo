c99734c123417896ec406ca8fda50fda114b86d4
mistiliyanov <mistiliyanov@salesforce.com>
2023-06-15 20:49:44 +0100

    update models

--- django_templates_demo/web/models.py
+++ django_templates_demo/web/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+# department name, slug (names + id), create date, update date
+class Department(models.Model):
+    pass
+
+
+class Employee(models.Model):
+    pass
+
+
+class Project(models.Model):
+    pass
+
+
+class SecurityCard(models.Model):
+    pass
